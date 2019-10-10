da1c0b712b7e4ac18d95dc0d3bda825438614032
Nate_NBJ <Nate_NBJ@localhost>
2019-10-10 11:16:33 -0600

    1.2.0

--- src/fuel_siphoning/ModPlugin.java
+++ src/fuel_siphoning/ModPlugin.java
@@ -2,22 +2,87 @@ package fuel_siphoning;
 
 import com.fs.starfarer.api.BaseModPlugin;
 import com.fs.starfarer.api.Global;
+import com.fs.starfarer.api.campaign.CargoAPI;
+import com.fs.starfarer.api.campaign.CargoStackAPI;
+import com.fs.starfarer.api.campaign.econ.MarketAPI;
+import com.fs.starfarer.api.impl.campaign.ids.Commodities;
+import com.fs.starfarer.api.impl.campaign.ids.Submarkets;
+import com.fs.starfarer.campaign.ui.trade.CargoItemStack;
+import com.fs.starfarer.loading.Oo0O;
+import org.json.JSONObject;
 
 public class ModPlugin extends BaseModPlugin {
+    public static final String ID = "sun_fuel_siphoning";
+    public static final String ABILITY_ID = "sun_fs_siphon_fuel";
+    public static final String SETTINGS_PATH = "FUEL_SIPHONING_OPTIONS.ini";
+
+    public static float
+        FUEL_CONSUMPTION_MULT = 1,
+        FUEL_VALUE = 25,
+        SENSOR_PROFILE_INCREASE_PERCENT = 3,
+        HIGH_DENSITY_CONVERSION_RATIO = 1,
+        LOW_DENSITY_CONVERSION_RATIO = 0.75f;
+
+    private static boolean settingsAlreadyRead = false;
+
     @Override
     public void afterGameSave() {
-        Global.getSector().getCharacterData().addAbility("sun_fs_siphon_fuel");
+        Global.getSector().getCharacterData().addAbility(ABILITY_ID);
     }
 
     @Override
     public void beforeGameSave() {
-        Global.getSector().getCharacterData().removeAbility("sun_fs_siphon_fuel");
+        Global.getSector().getCharacterData().removeAbility(ABILITY_ID);
     }
 
     @Override
     public void onGameLoad(boolean newGame) {
-        if(!Global.getSector().getPlayerFleet().hasAbility("sun_fs_siphon_fuel")) {
-            Global.getSector().getCharacterData().addAbility("sun_fs_siphon_fuel");
+        try {
+            if(!Global.getSector().getPlayerFleet().hasAbility(ABILITY_ID)) {
+                Global.getSector().getCharacterData().addAbility(ABILITY_ID);
+            }
+
+            Oo0O fuelSpec = ((Oo0O)Global.getSector().getEconomy().getCommoditySpec(Commodities.FUEL));
+
+            if(!settingsAlreadyRead) {
+                JSONObject cfg = Global.getSettings().getMergedJSONForMod(SETTINGS_PATH, ID);
+
+                FUEL_CONSUMPTION_MULT = (float) cfg.getDouble("fuelConsumptionMult");
+                FUEL_VALUE = (float) cfg.getDouble("fuelValue");
+                SENSOR_PROFILE_INCREASE_PERCENT = (float) cfg.getDouble("sensorProfileIncreasePercent");
+                HIGH_DENSITY_CONVERSION_RATIO = (float) cfg.getDouble("highDensityConversionRatio");
+                LOW_DENSITY_CONVERSION_RATIO = (float) cfg.getDouble("lowDensityConversionRatio");
+
+                settingsAlreadyRead = true;
+            }
+
+            Global.getSector().getPlayerFleet().getStats().getFuelUseHyperMult().modifyMult("sun_fs_fuel_mult", FUEL_CONSUMPTION_MULT);
+
+            fuelSpec.setBasePrice(FUEL_VALUE);
+
+            updateBaseValueOfFuel(Global.getSector().getPlayerFleet().getCargo());
+
+            for (MarketAPI market : Global.getSector().getEconomy().getMarketsCopy()) {
+                updateBaseValueOfFuel(market.getSubmarket(Submarkets.SUBMARKET_STORAGE).getCargo());
+            }
+        } catch (Exception e) {
+            String stackTrace = "";
+
+            for(int i = 0; i < e.getStackTrace().length; i++) {
+                StackTraceElement ste = e.getStackTrace()[i];
+                stackTrace += "    " + ste.toString() + System.lineSeparator();
+            }
+
+            Global.getLogger(ModPlugin.class).error(e.getMessage() + System.lineSeparator() + stackTrace);
         }
     }
+
+    public void updateBaseValueOfFuel(CargoAPI cargo) {
+        for(CargoStackAPI stack : cargo.getStacksCopy()) {
+            if(stack.isFuelStack() && stack.getBaseValuePerUnit() != FUEL_VALUE) {
+                ((CargoItemStack)stack).readResolve();
+            }
+        }
+    }
+
 }
--- src/fuel_siphoning/campaign/abilities/SiphonFuelAbility.java
+++ src/fuel_siphoning/campaign/abilities/SiphonFuelAbility.java
@@ -4,14 +4,11 @@ import java.awt.Color;
 import java.util.EnumSet;
 
 import com.fs.starfarer.api.campaign.*;
-import com.fs.starfarer.api.impl.campaign.terrain.StarCoronaTerrainPlugin;
-import org.lwjgl.opengl.GL11;
-import org.lwjgl.util.vector.Vector2f;
+import com.fs.starfarer.api.fleet.FleetMemberViewAPI;
+import fuel_siphoning.ModPlugin;
 
 import com.fs.starfarer.api.Global;
 import com.fs.starfarer.api.campaign.econ.CommoditySpecAPI;
-import com.fs.starfarer.api.combat.ViewportAPI;
-import com.fs.starfarer.api.graphics.SpriteAPI;
 import com.fs.starfarer.api.impl.campaign.abilities.BaseToggleAbility;
 import com.fs.starfarer.api.impl.campaign.ids.Commodities;
 import com.fs.starfarer.api.ui.LabelAPI;
@@ -19,21 +16,17 @@ import com.fs.starfarer.api.ui.TooltipMakerAPI;
 import com.fs.starfarer.api.util.Misc;
 
 public class SiphonFuelAbility extends BaseToggleAbility {
-	static void log(String message) { if(true) Global.getLogger(SiphonFuelAbility.class).info(message); }
-
-    public static final String SUPPLIES_ID = Commodities.SUPPLIES;
-    public static final String FUEL_ID = Commodities.FUEL;
-    public static final float DETECTABILITY_PERCENT = 100f;
+	public static final Color CONTRAIL_COLOR = new Color(255, 97, 27, 80);
 
     public float getFuelPerSupply() {
         return 1.00f
-                * Global.getSector().getEconomy().getCommoditySpec(SUPPLIES_ID).getBasePrice()
-                / Global.getSector().getEconomy().getCommoditySpec(FUEL_ID).getBasePrice();
+                * Global.getSector().getEconomy().getCommoditySpec(Commodities.SUPPLIES).getBasePrice()
+                / Global.getSector().getEconomy().getCommoditySpec(Commodities.FUEL).getBasePrice();
     }
     
     @Override
     protected String getActivationText() {
-        if (SUPPLIES_ID != null && getFleet() != null
+        if (Commodities.SUPPLIES != null && getFleet() != null
                 && (getFleet().getCargo().getSupplies() <= 0
                 || getFleet().getCargo().getFuel() >= getFleet().getCargo().getMaxFuel())) {
             return null;
@@ -48,66 +41,61 @@ public class SiphonFuelAbility extends BaseToggleAbility {
 
     @Override
     public void createTooltip(TooltipMakerAPI tooltip, boolean expanded) {
-            Color gray = Misc.getGrayColor();
-            Color highlight = Misc.getHighlightColor();
+        Color gray = Misc.getGrayColor();
+        Color highlight = Misc.getHighlightColor();
 
-            String status = " (off)";
-            if (turnedOn) {
-                    status = " (on)";
-            }
+        String status = " (off)";
+        if (turnedOn) {
+                status = " (on)";
+        }
 
-            LabelAPI title = tooltip.addTitle(spec.getName() + status);
-            title.highlightLast(status);
-            title.setHighlightColor(gray);
+        LabelAPI title = tooltip.addTitle(spec.getName() + status);
+        title.highlightLast(status);
+        title.setHighlightColor(gray);
 
-            float pad = 10f;
-            tooltip.addPara("Synthesize fuel using plasma collected from the corona of a red giant star.", pad);
+        float pad = 10f;
+        tooltip.addPara("Synthesize fuel using the trace antimatter and heavy isotopes of hydrogen found in nebulae.", pad);
 
-            if (!hasFuelSource()) {
-                    tooltip.addPara("Your fleet is currently not within the corona of a red giant or red supergiant star.", Misc.getNegativeHighlightColor(), pad);
-            } else {
-                    tooltip.addPara("Your fleet is within the corona of a red giant star and can begin siphoning fuel.", Misc.getPositiveHighlightColor(), pad);
-            }
+        if (!hasFuelSource()) {
+            tooltip.addPara("Your fleet must be within a nebula in order to siphon fuel.", Misc.getNegativeHighlightColor(), pad);
+        } else {
+            boolean inNebulaSystem = getFleet().getContainingLocation().isNebula();
+            Color clr = inNebulaSystem ? Misc.getPositiveHighlightColor() : Misc.getHighlightColor();
+            String density = (inNebulaSystem ? "high" : "low") + " density nebula";
+            String fuelPerSupply = Misc.getRoundedValueMaxOneAfterDecimal(getFuelPerSupply()
+                    * (inNebulaSystem ? ModPlugin.HIGH_DENSITY_CONVERSION_RATIO : ModPlugin.LOW_DENSITY_CONVERSION_RATIO));
+            String canOrIs = isActive() ? "is siphoning" : "can siphon";
 
-            tooltip.addPara("Increases the range at which the fleet can be detected by %s and consumes one unit of supplies for every %s units of fuel.",
-                            pad, highlight,
-                            "" + (int)DETECTABILITY_PERCENT + "%",
-                            "" + Misc.getRoundedValueMaxOneAfterDecimal(getFuelPerSupply())
-            );
+            tooltip.addPara("Your fleet is within a %s and " + canOrIs + " %s fuel per unit of supplies.",
+                        pad, Misc.getTextColor(), clr, density, fuelPerSupply);
+        }
+
+        tooltip.addPara("Increases the range at which the fleet can be detected by %s and consumes supplies in exchange for fuel.",
+                        pad, highlight, (int)ModPlugin.SENSOR_PROFILE_INCREASE_PERCENT + "%");
 
 
-            addIncompatibleToTooltip(tooltip, expanded);
+        addIncompatibleToTooltip(tooltip, expanded);
     }
 
     @Override
     public boolean hasTooltip() { return true; }
 
     @Override
-    public EnumSet<CampaignEngineLayers> getActiveLayers() {
-            return EnumSet.of(CampaignEngineLayers.ABOVE);
-    }
-
-    private float phaseAngle;
-    @Override
     protected void applyEffect(float amount, float level) {
         CampaignFleetAPI fleet = getFleet();
         if (fleet == null) return;
-
-        fleet.getStats().getDetectedRangeMod().modifyPercent(getModId(), DETECTABILITY_PERCENT * level, "Siphoning fuel");
-        //fleet.getStats()
-        
-        float days = Global.getSector().getClock().convertToDays(amount);
-        phaseAngle += days * 360f * 10f;
-        phaseAngle = Misc.normalizeAngle(phaseAngle);
         
         if(!isActive()) return;
-        
-        float cost = days * fleet.getCargo().getMaxFuel() * level / getFuelPerSupply();
-        float fuel = fleet.getCargo().getCommodityQuantity(FUEL_ID);
-        
+
+        fleet.getStats().getDetectedRangeMod().modifyPercent(getModId(), ModPlugin.SENSOR_PROFILE_INCREASE_PERCENT, "Siphoning fuel");
+
+        float days = Global.getSector().getClock().convertToDays(amount);
+        float cost = days * fleet.getCargo().getMaxFuel() * (level / getFuelPerSupply()) * (fleet.getCurrBurnLevel() / 10);
+        float fuel = fleet.getCargo().getCommodityQuantity(Commodities.FUEL);
+
         if (!hasFuelSource()) {
             deactivate();
-        } else if(fleet.getCargo().getCommodityQuantity(SUPPLIES_ID) <= 0) {
+        } else if(fleet.getCargo().getCommodityQuantity(Commodities.SUPPLIES) <= 0) {
             CommoditySpecAPI spec = getCommodity();
             fleet.addFloatingText("Out of " + spec.getName().toLowerCase(), Misc.setAlpha(entity.getIndicatorColor(), 255), 0.5f);
             deactivate();
@@ -116,15 +104,18 @@ public class SiphonFuelAbility extends BaseToggleAbility {
             fleet.addFloatingText("Full of fuel", Misc.setAlpha(entity.getIndicatorColor(), 255), 0.5f);
             deactivate();
         } else {
-            fleet.getCargo().removeCommodity(SUPPLIES_ID, cost);
-            fleet.getCargo().addCommodity(FUEL_ID, cost * getFuelPerSupply());
-        }
+            fleet.getCargo().removeCommodity(Commodities.SUPPLIES, cost);
+            fleet.getCargo().addCommodity(Commodities.FUEL, cost * getFuelPerSupply());
 
-        
+			for (FleetMemberViewAPI view : getFleet().getViews()) {
+				view.getContrailColor().shift("sun_fs_wake", CONTRAIL_COLOR, getActivationDays(), 2, 1f);
+				view.getContrailWidthMult().shift("sun_fs_wake", 6, getActivationDays(), 2, 1f);
+			}
+        }
     }
 
     public CommoditySpecAPI getCommodity() {
-            return Global.getSettings().getCommoditySpec(SUPPLIES_ID);
+        return Global.getSettings().getCommoditySpec(Commodities.SUPPLIES);
     }
 
     @Override
@@ -133,30 +124,7 @@ public class SiphonFuelAbility extends BaseToggleAbility {
     }
 
     protected boolean hasFuelSource() {
-        CampaignFleetAPI fleet = getFleet();
-        if (fleet == null) return false;
-
-        float radius = 0;
-
-        for(CampaignTerrainAPI terrain : fleet.getContainingLocation().getTerrainCopy()) {
-        	if(terrain.getPlugin() instanceof StarCoronaTerrainPlugin) {
-				StarCoronaTerrainPlugin corona = (StarCoronaTerrainPlugin)terrain.getPlugin();
-
-				if(corona.containsEntity(fleet)) {
-					radius = Math.max(radius, corona.getAuroraOuterRadius());
-				}
-			}
-		}
-
-        if(radius != 0) {
-			for (PlanetAPI planet : fleet.getContainingLocation().getPlanets()) {
-				if((planet.getTypeId().contains("star_red_giant") || planet.getTypeId().contains("star_red_supergiant"))
-						&& Misc.getDistance(planet, fleet) < (fleet.getRadius() + planet.getRadius() + radius)) {
-					return true;
-				}
-			}
-		}
-        return false;
+    	return getFleet().getStats().getDetectedRangeMod().getMultBonuses().containsKey("nebula_stat_mod_1");
     }
 
     @Override
@@ -164,234 +132,9 @@ public class SiphonFuelAbility extends BaseToggleAbility {
 
     @Override
     protected void cleanupImpl() {
-            CampaignFleetAPI fleet = getFleet();
-            if (fleet == null) return;
+        CampaignFleetAPI fleet = getFleet();
+        if (fleet == null) return;
 
-            fleet.getStats().getDetectedRangeMod().unmodify(getModId());
+        fleet.getStats().getDetectedRangeMod().unmodify(getModId());
     }
-
-    transient private SpriteAPI texture;
-    @Override
-    public void render(CampaignEngineLayers layer, ViewportAPI viewport) {
-		float level = getProgressFraction();
-		if (level <= 0) return;
-		if (getFleet() == null) return;
-		if (!getFleet().isPlayerFleet()) return;
-		
-		float alphaMult = viewport.getAlphaMult() * level;
-		
-//		float x = getFleet().getLocation().x;
-//		float y = getFleet().getLocation().y;
-//		
-//		GL11.glPushMatrix();
-//		GL11.glTranslatef(x, y, 0);
-//		
-//		GL11.glDisable(GL11.GL_TEXTURE_2D);
-//		Misc.renderQuad(30, 30, 100, 100, Color.green, alphaMult * level);
-//		
-//		
-//		GL11.glPopMatrix();
-	
-		
-		//float noiseLevel = data.getNoiseLevel();
-		
-		float bandWidthInTexture = 256;
-		float bandIndex;
-		
-		float radStart = getFleet().getRadius() + 5;
-		float radEnd = radStart + 50f;
-		
-		float circ = (float) (Math.PI * 2f * (radStart + radEnd) / 2f);
-		//float pixelsPerSegment = 10f;
-		float pixelsPerSegment = circ / 360f;
-		//float pixelsPerSegment = circ / 720;
-		float segments = Math.round(circ / pixelsPerSegment);
-		
-//		segments = 360;
-//		pixelsPerSegment = circ / segments;
-		//pixelsPerSegment = 10f;
-		
-		float startRad = (float) Math.toRadians(0);
-		float endRad = (float) Math.toRadians(360f);
-		float spanRad = Math.abs(endRad - startRad);
-		float anglePerSegment = spanRad / segments;
-		
-		Vector2f loc = getFleet().getLocation();
-		float x = loc.x;
-		float y = loc.y;
-
-		
-		GL11.glPushMatrix();
-		GL11.glTranslatef(x, y, 0);
-		
-		//float zoom = viewport.getViewMult();
-		//GL11.glScalef(zoom, zoom, 1);
-		
-		GL11.glEnable(GL11.GL_TEXTURE_2D);
-		
-		if (texture == null) texture = Global.getSettings().getSprite("abilities", "neutrino_detector");
-		texture.bindTexture();
-		
-		GL11.glEnable(GL11.GL_BLEND);
-		//GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE);
-		GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
-		
-		boolean outlineMode = false;
-		//outlineMode = true;
-		if (outlineMode) {
-			GL11.glDisable(GL11.GL_TEXTURE_2D);
-			GL11.glDisable(GL11.GL_BLEND);
-			GL11.glPolygonMode(GL11.GL_FRONT_AND_BACK, GL11.GL_LINE);
-			//GL11.glPolygonMode(GL11.GL_FRONT_AND_BACK, GL11.GL_FILL);
-		}
-		
-		float thickness = (radEnd - radStart) * 1f;
-		float radius = radStart;
-
-		float texProgress = 0f;
-		float texHeight = texture.getTextureHeight();
-		float imageHeight = texture.getHeight();
-		float texPerSegment = pixelsPerSegment * texHeight / imageHeight * bandWidthInTexture / thickness;
-		
-		texPerSegment *= 1f;
-		
-		float totalTex = Math.max(1f, Math.round(texPerSegment * segments));
-		texPerSegment = totalTex / segments;
-		
-		float texWidth = texture.getTextureWidth();
-		float imageWidth = texture.getWidth();
-		
-		
-		
-		Color color = new Color(255,25,25,195);
-		//Color color = new Color(255,25,255,155);
-		
-		
-		for (int iter = 0; iter < 2; iter++) {
-			if (iter == 0) {
-				bandIndex = 1;
-			} else {
-				//color = new Color(255,215,25,255);
-				//color = new Color(25,255,215,255);
-				bandIndex = 0;
-				texProgress = segments/2f * texPerSegment;
-				//GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE);
-			}
-			if (iter == 1) {
-				GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE);
-			}
-			//bandIndex = 1;
-			
-			float leftTX = (float) bandIndex * texWidth * bandWidthInTexture / imageWidth;
-			float rightTX = (float) (bandIndex + 1f) * texWidth * bandWidthInTexture / imageWidth - 0.001f;
-			
-			GL11.glBegin(GL11.GL_QUAD_STRIP);
-			for (float i = 0; i < segments + 1; i++) {
-				
-				float segIndex = i % (int) segments;
-				
-				//float phaseAngleRad = (float) Math.toRadians(phaseAngle + segIndex * 10) + (segIndex * anglePerSegment * 10f);
-				float phaseAngleRad;
-				if (iter == 0) {
-					phaseAngleRad = (float) Math.toRadians(phaseAngle) + (segIndex * anglePerSegment * 29f);
-				} else { //if (iter == 1) { 
-					phaseAngleRad = (float) Math.toRadians(-phaseAngle) + (segIndex * anglePerSegment * 17f);
-				}
-				
-				
-				float angle = (float) Math.toDegrees(segIndex * anglePerSegment);
-				//if (iter == 1) angle += 180;
-				
-				
-				float pulseSin = (float) Math.sin(phaseAngleRad);
-				float pulseMax = thickness * 0.5f;
-
-				pulseMax = thickness * 0.2f;
-				pulseMax = 10f;
-				
-				//pulseMax *= 0.25f + 0.75f * noiseLevel;
-				
-				float pulseAmount = pulseSin * pulseMax;
-				//float pulseInner = pulseAmount * 0.1f;
-				float pulseInner = pulseAmount * 0.1f;
-				
-				float r = radius;
-
-//				float thicknessMult = delegate.getAuroraThicknessMult(angle);
-//				float thicknessFlat = delegate.getAuroraThicknessFlat(angle);
-				
-				float theta = anglePerSegment * segIndex;;
-				float cos = (float) Math.cos(theta);
-				float sin = (float) Math.sin(theta);
-				
-				float rInner = r - pulseInner;
-				//if (rInner < r * 0.9f) rInner = r * 0.9f;
-				
-				//float rOuter = (r + thickness * thicknessMult - pulseAmount + thicknessFlat);
-				float rOuter = r + thickness - pulseAmount;
-				
-				
-				//rOuter += noiseLevel * 25f;
-				
-                                float grav = 1;
-				//float grav = data.getDataAt(angle);
-				//if (grav > 500) System.out.println(grav);
-				//if (grav > 300) grav = 300;
-				if (grav > 750) grav = 750;
-				grav *= 250f / 750f;
-				grav *= level;
-				//grav *= 0.5f;
-				//rInner -= grav * 0.25f;
-				
-				//rInner -= grav * 0.1f;
-				rOuter += grav;
-//				rInner -= grav * 3f;
-//				rOuter -= grav * 3f;
-				//System.out.println(grav);
-				
-				float alpha = alphaMult;
-				alpha *= 0.25f + Math.min(grav / 100, 0.75f);
-				//alpha *= 0.75f;
-				
-//			
-//				
-//				
-//				phaseAngleWarp = (float) Math.toRadians(phaseAngle - 180 * iter) + (segIndex * anglePerSegment * 1f);
-//				float warpSin = (float) Math.sin(phaseAngleWarp);
-//				rInner += thickness * 0.5f * warpSin;
-//				rOuter += thickness * 0.5f * warpSin;
-				
-				
-				
-				float x1 = cos * rInner;
-				float y1 = sin * rInner;
-				float x2 = cos * rOuter;
-				float y2 = sin * rOuter;
-				
-				x2 += (float) (Math.cos(phaseAngleRad) * pixelsPerSegment * 0.33f);
-				y2 += (float) (Math.sin(phaseAngleRad) * pixelsPerSegment * 0.33f);
-				
-				
-				GL11.glColor4ub((byte)color.getRed(),
-						(byte)color.getGreen(),
-						(byte)color.getBlue(),
-						(byte)((float) color.getAlpha() * alphaMult * alpha));
-				
-				GL11.glTexCoord2f(leftTX, texProgress);
-				GL11.glVertex2f(x1, y1);
-				GL11.glTexCoord2f(rightTX, texProgress);
-				GL11.glVertex2f(x2, y2);
-				
-				texProgress += texPerSegment * 1f;
-			}
-			GL11.glEnd();
-			
-			//GL11.glRotatef(180, 0, 0, 1);
-		}
-		GL11.glPopMatrix();
-		
-		if (outlineMode) {
-			GL11.glPolygonMode(GL11.GL_FRONT_AND_BACK, GL11.GL_FILL);
-		}
-	}
 }
